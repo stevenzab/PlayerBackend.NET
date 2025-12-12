494d00d0fac2ab988562ceb118ffe550f3214d12
stevenzab <steven.zabandith@epitech.eu>
2025-12-12 19:25:54 +0100

    [ADD] PlayerModel

---
+++ src/PlayerBack.Domain/Models/Player.cs
@@ -0,0 +1,43 @@
+﻿namespace PlayerBack.Domain.Models
+{
+    public class Player : RepositoryCollection
+    {
+        public int PlayerId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Shortname { get; set; }
+
+        public string Sex { get; set; }
+
+        public Country Country { get; set; }
+
+        public string Picture { get; set; }
+
+        public PlayerData Data { get; set; }
+    }
+
+    public class Country
+    {
+        public string Picture { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class PlayerData
+    {
+        public int Rank { get; set; }
+
+        public int Points { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int Age { get; set; }
+
+        public List<int> Last { get; set; }
+    }
+}
---
+++ src/PlayerBack.Domain/Models/RepositoryCollection.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PlayerBack.Domain.Models
+{
+    public class RepositoryCollection
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; }
+    }
+}
