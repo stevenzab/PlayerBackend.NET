5907a61022993676afc0d430bc76b15ed8c59599
stevenzab <steven.zabandith@epitech.eu>
2025-12-12 19:24:51 +0100

    [ADD] DTO

---
+++ src/PlayerBack.Domain/Dtos/PlayerDto.cs
@@ -0,0 +1,45 @@
+﻿namespace PlayerBack.Domain.Dtos
+{
+    using System.Collections.Generic;
+
+    public class PlayerDto
+    {
+        public int PlayerId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Shortname { get; set; }
+
+        public string Sex { get; set; }
+
+        public CountryDto Country { get; set; }
+
+        public string Picture { get; set; }
+
+        public PlayerDataDto Data { get; set; }
+    }
+
+    public class CountryDto
+    {
+        public string Picture { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class PlayerDataDto
+    {
+        public int Rank { get; set; }
+
+        public int Points { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int Age { get; set; }
+
+        public List<int> Last { get; set; }
+    }
+}
