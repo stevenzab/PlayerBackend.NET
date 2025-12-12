fdbd050dd0bc8aaad7f4edd99abab6e9fe9cdf46
stevenzab <steven.zabandith@epitech.eu>
2025-12-12 20:17:23 +0100

    [DEL] non used repository

--- src/PlayerBack.Infrastructure/Repository/IPlayerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using PlayerBack.Infrastructure.Common;
-using PlayerBack.Infrastructure.Models;
-
-namespace PlayerBack.Infrastructure.Repository
-{
-    public interface IPlayerRepository : IBaseRepository<Player>
-    {
-    }
-}
