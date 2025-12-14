bc3a3063610cfcb961434d8467308759cbfd3993
stevenzab <steven.zabandith@epitech.eu>
2025-12-14 14:49:52 +0100

    [ADD] model

---
+++ src/PlayerBack.Domain/Models/StatisticsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerBack.Domain.Models
+{
+    public class StatisticsModel
+    {
+        public string? CountryCodeWithHighestWinRatio { get; set; }
+        public double HighestWinRatio { get; set; }
+        public double AverageBmi { get; set; }
+        public double MedianHeight { get; set; }
+    }
+}
