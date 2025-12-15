e95cda419b27b1f4d2f7c8bc2c1b0b8772e26c91
stevenzab <steven.zabandith@epitech.eu>
2025-12-15 01:17:51 +0100

    [ADD] unit test infra

---
+++ test/PlayerBack.Infrastructure.UnitTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayerBack.Infrastructure.UnitTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
