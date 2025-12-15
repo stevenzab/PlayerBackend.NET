1fa182b4f0f881b712eeb1607b2f7fd246ade714
stevenzab <steven.zabandith@epitech.eu>
2025-12-15 14:44:51 +0100

    [ADD] middleware unit tests

---
+++ test/PlayerBack.Api.UnitTests/Middleware/GlobalExceptionHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PlayerBack.Api.Middleware;
+using System;
+using System.Threading.Tasks;
+
+namespace PlayerBack.Api.UnitTests.Middleware
+{
+    [TestClass]
+    public class GlobalExceptionHandlerTests
+    {
+        private MockRepository mockRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Default);
+        }
+
+        private GlobalExceptionHandler CreateGlobalExceptionHandler()
+        {
+            return new GlobalExceptionHandler();
+        }
+
+        [TestMethod]
+        public async Task TryHandleAsync_SetsResponse_ForGenericException()
+        {
+            // Arrange
+            var handler = CreateGlobalExceptionHandler();
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            var exception = new Exception("Something went wrong");
+
+            // Act
+            var result = await handler.TryHandleAsync(context, exception, CancellationToken.None);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(StatusCodes.Status500InternalServerError, context.Response.StatusCode);
+        }
+    }
+}
